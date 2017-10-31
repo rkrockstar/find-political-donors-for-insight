6a731b449a61e6311588d629019248f27e939368
LAPTOP-Rounak Kulkarni <rounakkulkarni@csu.fullerton.edu>
2017-10-31 12:20:52 -0700

    Version 5

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@ clear
 echo "Starting Shell Script"
 ls
 cd \src
+echo "Python Version"
 python -c 'import sys; print(sys.version_info[:])'
+python get-pip.py
+pip -V
+pip install numpy
 python sourcefile.py
 echo "Shell Script Executed"
 cat run.sh
